ca8e9702c809314648d980d3dea26ef931700c0f
Kajol232 <ridamot123@yahoo.com>
2020-10-28 15:30:15 +0100

    Initial commit

---
+++ app/src/main/java/muhammad/programming/co/rockpaperscissors/MainActivity.java
@@ -0,0 +1,14 @@
+package muhammad.programming.co.rockpaperscissors;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
